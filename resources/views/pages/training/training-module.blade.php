616b5ab6a513b67af02291915f5b2fdde0c94538
Muhammad Surya Saniansyah <muhammadsurya216@gmail.com>
2025-09-19 08:46:47 +0800

    feat(training/module): improve loading while importing

--- resources/views/pages/training/training-module.blade.php
+++ resources/views/pages/training/training-module.blade.php
@@ -12,10 +12,19 @@
             <div class="flex items-center justify-center gap-2">
                 <x-dropdown no-x-anchor right>
                     <x-slot:trigger>
-                        <x-button icon="o-clipboard-document-list" class="btn-success h-9">Excel</x-button>
+                        <x-button class="btn-success h-10" wire:target="file" wire:loading.attr="disabled">
+                            <span class="flex items-center gap-2" wire:loading.remove wire:target="file">
+                                <x-icon name="o-clipboard-document-list" class="size-4" />
+                                Excel
+                            </span>
+                            <span class="flex items-center gap-2" wire:loading wire:target="file">
+                                <x-icon name="o-arrow-path" class="size-4 animate-spin" />
+                            </span>
+                        </x-button>
                     </x-slot:trigger>
 
-                    <x-menu-item title="Export" icon="o-arrow-down-on-square" wire:click.stop="export" spinner="export" />
+                    <x-menu-item title="Export" icon="o-arrow-down-on-square" wire:click.stop="export"
+                        spinner="export" />
 
                     <label class="w-full cursor-pointer">
                         <x-menu-item title="Import" icon="o-arrow-up-on-square" />
