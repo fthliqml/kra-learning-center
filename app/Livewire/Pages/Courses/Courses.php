a8600e7cf21ae6fdfdbd9f0f1d8f2f5bd2317f54
Muhammad Surya Saniansyah <muhammadsurya216@gmail.com>
2025-09-24 16:50:40 +0800

    feat: add courses page

--- app/Livewire/Pages/Courses/Courses.php
+++ app/Livewire/Pages/Courses/Courses.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class Courses extends Component
 {
+    public $groupOptions = [
+        ['value' => 'all', 'label' => 'All Groups'],
+        ['value' => 'group1', 'label' => 'Group 1'],
+        ['value' => 'group2', 'label' => 'Group 2'],
+    ];
+
     public function render()
     {
         return view('pages.courses.courses');
--- resources/views/pages/courses/courses.blade.php
+++ resources/views/pages/courses/courses.blade.php
@@ -1,3 +1,22 @@
 <div>
-    {{-- Stop trying to control. --}}
+
+    {{-- Header --}}
+    <div class="w-full grid gap-10 lg:gap-5 mb-5 lg:mb-9
+                grid-cols-1 lg:grid-cols-2 items-center">
+        <h1 class="text-primary text-4xl font-bold text-center lg:text-start">
+            Courses
+        </h1>
+
+        <div class="flex gap-3 flex-col w-full items-center justify-center lg:justify-end md:gap-4 md:flex-row">
+            <x-search-input placeholder="Search..." class="max-w-72" input-class="!rounded-full "
+                wire:model.live="search" />
+
+            <div class="flex items-center justify-center gap-2">
+                <x-select wire:model.live="filter" :options="$groupOptions" option-value="value" option-label="label"
+                    placeholder="Filter"
+                    class="!w-30 !h-10 focus-within:border-0 hover:outline-1 focus-within:outline-1 cursor-pointer rounded-full shadow-sm [&_svg]:!opacity-100"
+                    icon-right="o-funnel" />
+            </div>
+        </div>
+    </div>
 </div>
