ebe5be2fd15a2c54999c5e33489d20b6eda6bf01
Muhammad Surya Saniansyah <muhammadsurya216@gmail.com>
2025-12-09 10:49:40 +0800

    feat: update DatabaseSeeder to include CompetencySeeder and comment out unused seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,18 +15,19 @@ class DatabaseSeeder extends Seeder
     {
         User::factory()->count(10)->create();
         $this->call(UserSeeder::class);
-        $this->call(TrainingModuleSeeder::class);
-        $this->call(TrainingSeeder::class);
-        $this->call(TrainingHistorySeeder::class);
-        $this->call(TrainingSurveySeeder::class);
-        $this->call(CoursesSeeder::class);
-        $this->call(TopicSeeder::class);
-        $this->call(SectionSeeder::class);
-        $this->call(ResourceSeeder::class);
-        $this->call(CourseAssignmentSeeder::class);
-        $this->call(SurveyTemplateSeeder::class);
-        $this->call(TrainingRequestSeeder::class);
-        $this->call(CertificationModuleSeeder::class);
-        $this->call(CertificationSeeder::class);
+        $this->call(CompetencySeeder::class);
+        // $this->call(TrainingModuleSeeder::class);
+        // $this->call(TrainingSeeder::class);
+        // $this->call(TrainingHistorySeeder::class);
+        // $this->call(TrainingSurveySeeder::class);
+        // $this->call(CoursesSeeder::class);
+        // $this->call(TopicSeeder::class);
+        // $this->call(SectionSeeder::class);
+        // $this->call(ResourceSeeder::class);
+        // $this->call(CourseAssignmentSeeder::class);
+        // $this->call(SurveyTemplateSeeder::class);
+        // $this->call(TrainingRequestSeeder::class);
+        // $this->call(CertificationModuleSeeder::class);
+        // $this->call(CertificationSeeder::class);
     }
 }
